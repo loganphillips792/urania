e8f91786c36ac1ba35a98a75f6a5e92f18e1a8b8
loganphillips792 <loganphillips792@gmail.com>
2022-04-23 12:50:28 -0400

    wip

--- backend/main.py
+++ backend/main.py
@@ -3,6 +3,7 @@ from flask import Flask
 from flask_cors import CORS
 import obd
 import logging
+import random
 
 app = Flask(__name__)
 CORS(app)
@@ -16,8 +17,8 @@ def hello_world():
 def metrics():
     app.logger.info("Logging")
     return {
-        "rpm": 2000,
-        "speed": 50
+        "rpm": random.randint(1000,10000),
+        "speed": random.randint(0, 200)
     }
 
 #def main():   
--- frontend/src/components/screens/HomeScreen.js
+++ frontend/src/components/screens/HomeScreen.js
@@ -1,4 +1,5 @@
-import { useState, useEffect } from 'react';
+import { useState, useEffect, useRef } from 'react';
+import { animate } from 'framer-motion';
 import styled from 'styled-components';
 
 const Container = styled.div`
@@ -10,24 +11,29 @@ const MetricsContainer = styled.div`
 `;
 
 const MetricContainer = styled.div`
-    width: 100px;
-    height: 100px;
     border: 2px solid orange;
     flex-grow: 1;
 `;
 
 const RPM = styled(MetricContainer)`
-   
 `;
 
 const Speed = styled(MetricContainer)`
-
 `;
 
+const MetricName = styled.div`
+    font-size: 100px;
+    text-align: center;
+`;
+
+const Number = styled.div`
+    font-size: 50px;
+    text-align: center;
+`;
 
 const HomeScreen = () => {
     const [metrics, setMetrics] = useState({});
-
+   
     useEffect(() => {
         fetch("http://127.0.0.1:5000/metrics")
             .then(response => response.json())
@@ -41,10 +47,21 @@ const HomeScreen = () => {
         <Container>
             <MetricsContainer>
                 <RPM>
-                    RPM: {metrics['rpm']}
+                    <MetricName>
+                        RPM    
+                    </MetricName> 
+                    <Number>
+                        {metrics['rpm']}
+                    </Number>
                 </RPM>
                 <Speed>
-                    SPEED: {metrics['speed']}
+                    <MetricName>
+                        SPEED
+                    </MetricName>
+                     
+                    <Number>
+                        {metrics['speed']}
+                    </Number>
                 </Speed>
             </MetricsContainer>
         </Container>
